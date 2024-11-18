9f71794e347bb4760d8d491f5c1eb6724cfe40f1
Saniya <saniyachandel8827@gmail.com>
2024-11-18 12:00:48 +0530

    Add project files.

---
+++ EKART – AN ECOMMERCE APPLICATION/AutoMapping/EKartMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace EKART___AN_ECOMMERCE_APPLICATION.AutoMapping
+{
+    public class EKartMapping:Profile
+    {
+        public EKartMapping()
+        {
+            
+        }
+    }
+}
